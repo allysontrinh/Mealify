6a1ec959f2e0ea9bba7d9f1c9c004620429e69fb
vitdoan <vtdoan@umass.edu>
2024-02-17 16:39:10 -0500

    add sample code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import SampleComponent from './components/SampleComponent';
 
 function App() {
   return (
     <div>
       Hello
+      <SampleComponent/>
     </div>
   );
 }
---
+++ src/components/SampleComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function SampleComponent() {
+    return <div>
+        This is a sample component
+    </div>
+}
