82e925b12125bce61c552b7504e06f79bc875d54
vitdoan <vtdoan@umass.edu>
2024-02-18 01:27:30 -0500

    add frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import AboutPage from './components/AboutPage';
+import ImageUploader from './components/ImageUploader';
+import { NavbarComp } from './components/NavbarComp';
 import SampleComponent from './components/SampleComponent';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      Hello
-      <SampleComponent/>
+      <NavbarComp/>
+      {/* <div>
+        <AboutPage/>
+        <SampleComponent/>
+        <ImageUploader/>
+      </div> */}
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<AboutPage/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
